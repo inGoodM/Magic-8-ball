3d7c08d2a44a6014a53db40e08b51ccda25a736f
Kholodovskyi Artem Igorevich <31437162+inGoodM@users.noreply.github.com>
2022-05-24 00:29:23 +0300

    Initial Commit

---
+++ Magic 8 ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 ball
+//
+//  Created by inGM on 24.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
